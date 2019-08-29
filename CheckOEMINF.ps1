f2e473567587641d1167c23ef6c93b5d23153c45
Chuck Fowler <cf@braingears.com>
2019-08-29 16:01:16 -0400

    CheckOEMINF-TF.ps1
    Change Results to True / False

--- CheckOEMINF.ps1
+++ CheckOEMINF.ps1
@@ -8,64 +8,50 @@
   It will check for oeminf only for Windows 7 and Windows 2008 R2 
 
 #>
-
-$OS = (Get-WmiObject Win32_OperatingSystem).Caption
-$System = Get-WmiObject -Class Win32_ComputerSystem
-$Computer = $env:COMPUTERNAME
-$Result = @()
-
-If ($System.Model -eq "Virtual Machine" ){
-    Write-Output "$Computer is a Virtual Machine - Not Applicable"
-    Exit
-}
-Else{
-if ($os -match "Windows 10" -or $os -match "Microsoft Windows 10 Enterprise Insider Preview") {
-    try {
-			$NICObjs = @() 
-			$ActiveNICs = get-wmiobject win32_networkadapter -ErrorAction Stop -filter "netconnectionstatus = 2" | select name
-			foreach ( $nic in $ActiveNICs ) {
-				 $NICObjs += get-wmiobject win32_PnPSignedDriver -ErrorAction Stop |
-                             where { $_.DeviceName -eq $nic.name -or 
-						             $_.Description -eq $nic.name -or 
-						             $_.FriendlyName -eq $nic.name }
-			}
-        } catch {
-			 Write-Output "Exception : $_.Exception.Message"
-			 Exit
+&{
+    $OS = (Get-WmiObject Win32_OperatingSystem).Caption
+    $System = Get-WmiObject -Class Win32_ComputerSystem
+    $Computer = $env:COMPUTERNAME
+    $Result = @()
+	$OEMINF = $False
+        try {
+                $NICObjs = @() 
+                $ActiveNICs = Get-WmiObject win32_networkadapter -ErrorAction Stop -filter "netconnectionstatus = 2" | Select-Object Name
+                foreach ( $NIC in $ActiveNICs ) {
+                     $NICObjs += get-wmiobject win32_PnPSignedDriver -ErrorAction Stop |
+                                 where { $_.DeviceName -eq $NIC.Name -or 
+                                         $_.Description -eq $NIC.Name -or 
+                                         $_.FriendlyName -eq $NIC.Name }
+                }
+            } catch {
+                 Write-Output "Exception : $_.Exception.Message"
+                 Exit
+            }
+        foreach ($NIC in $NICObjs ){
+               if ( $NIC.InfName -match "^oe[m]") {
+                      $infPath = Join-Path $env:windir "\inf\$($NIC.InfName)"
+                      if (Test-Path  $infPath) {
+                          $Properties = @{ Computer = $Computer
+                                           OS = $OS
+                                           NIC = $NIC.DeviceName
+                                           OEMINF  = "Present"
+                                        }
+                          $Result += New-Object -TypeName PSObject $Properties
+                          $OEMINF = $True
+                      } Else {
+                          $Properties = @{ Computer = $Computer
+                                           OS = $OS
+                                           NIC = $NIC.DeviceName
+                                           OEMINF  = "Missing" }
+                          $Result += New-Object -TypeName PSObject $Properties 
+                      }
+               }Else {
+                     $Properties = @{ Computer = $Computer
+                                      OS = $OS
+                                      NIC = $NIC.DeviceName
+                                      OEMINF  = "Missing" }
+                     $Result += New-Object -TypeName PSObject $Properties     
+               }
         }
-    foreach ($nic in $NICObjs ){
-           if ( $nic.InfName -match "^oe[m]") {
-                  $infPath = Join-Path $env:windir "\inf\$($nic.InfName)"
-                  if (Test-Path  $infPath) {
-                      $Properties = @{ Computer = $Computer
-                                       OS = $OS
-                                       NIC = $nic.DeviceName
-                                       OEMINF  = "Present" }
-                      $Result += New-Object -TypeName PSObject $Properties
-                  } Else {
-                      $Properties = @{ Computer = $Computer
-                                       OS = $OS
-                                       NIC = $nic.DeviceName
-                                       OEMINF  = "Missing" }
-                      $Result += New-Object -TypeName PSObject $Properties 
-                
-                  }  
-      
-           }Else {
-                 $Properties = @{ Computer = $Computer
-                                  OS = $OS
-                                  NIC = $nic.DeviceName
-                                  OEMINF  = "Missing" }
-                 $Result += New-Object -TypeName PSObject $Properties     
-           } 
- 
-    }
-	
-	Write-Output $Result
-} 
-Else { 
-	Write-Output "Operating System $OS is Not Applicable" 
-    Exit
-}
-	
+Write-Output $OEMINF
 }
